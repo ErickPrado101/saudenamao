13c03e807d402614f7ce123a07064dbbf16a4666
ErickPrado101 <epg1.prado@gmail.com>
2025-01-28 11:49:40 -0300

    lp2

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react"
+import { Facebook, Instagram, Linkedin, Mail } from "lucide-react"
 
 const Contact = () => {
   return (
@@ -6,18 +6,18 @@ const Contact = () => {
       <div className="absolute inset-0 opacity-30 bg-[url('/circuit.svg')] bg-center"></div>
       <div className="container mx-auto text-center relative z-10">
         <h2 className="text-4xl font-bold mb-8 bg-clip-text text-transparent bg-gradient-to-r from-white to-[#3E8400]">
-          Conecte-se ao Futuro da Saúde
+          Conecte-se ao futuro da saúde
         </h2>
         <a
-          href="https://wa.me/5511999999999"
+          href="https://wa.me/556181926283"
           target="_blank"
           rel="noopener noreferrer"
           className="inline-block bg-gradient-to-r from-[#3E8400] to-[#006A3A] text-white px-8 py-3 rounded-full text-lg font-semibold hover:from-[#006A3A] hover:to-[#3E8400] transition-all duration-300 transform hover:scale-105 shadow-lg hover:shadow-xl mb-12"
         >
-          Inicie uma Conversa Futurista no WhatsApp
+          Inicie uma Conversa conodco no WhatsApp
         </a>
         <div className="flex justify-center space-x-6">
-          {[Facebook, Instagram, Twitter, Linkedin].map((Icon, index) => (
+          {[Facebook, Instagram, Mail, Linkedin].map((Icon, index) => (
             <a
               key={index}
               href="#"
